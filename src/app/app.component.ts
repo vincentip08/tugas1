3ab53e03927815d5d61b61d50f686e0cd8fff731
vincentip08 <c14180020@john.petra.ac.id>
2020-04-15 10:36:46 +0700

    final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,36 +16,28 @@ export class AppComponent  {
   saveVariables(a, b){
     this.hasil = Math.pow(a, b);
     if(this.hasil % 2 == 0){
-      this.show = true;
-      this.generateGenap(a, b, this.hasil);
+      for(var i =0; i<parseInt(b);i++){
+        this.temp = this.temp + "*";
+      }
+      this.arrPola.push(this.temp);
+      for(var i=parseInt(b); i<=this.hasil;i++){
+        this.temp = this.temp+"*";
+        this.arrPola.push(this.temp);
+      } 
     }
     else {
-      this.show = false;
-      this.generateGanjil(a, b, this.hasil);
-    }
-  }
-
-  generateGenap(a, b, hasil){
-    for(var i =0; i<b;i++){
-      this.temp = this.temp + "*";
-    }
-    this.arrPola.push(this.temp);
-    for(var i=parseInt(b); i<=this.hasil;i++){
-      this.temp = this.temp+"*";
-      this.arrPola.push(this.temp);
-    }
-  }
-
-  generateGanjil(a, b, hasil){
-    if(parseInt(a)<=parseInt(b)){
+      if(parseInt(a)<=parseInt(b)){
     
         for(var i = parseInt(b);i>0;i--){
           this.temp1='';
           for(var j=0;j<i;j++){
             this.temp1 = this.temp1 +"*";
             }
+          
         }
       }
     }
+    }
   }
+
 
